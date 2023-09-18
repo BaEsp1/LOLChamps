5ed12f190138e29d745aa5bcdc056f86e4b4a8a8
BaEsp1 <97913060+BaEsp1@users.noreply.github.com>
2023-09-18 19:05:10 -0300

    Add fonts

--- Front/src/Views/Lp.js
+++ Front/src/Views/Lp.js
@@ -1,8 +1,18 @@
+import Logo from "../lol/Logo.png"
+import "./Lp.css"
 
 function Lp () {
 
     return (
-        <h1>Welcome to Lol Champs</h1>
+        <div className="Lp">
+
+            <div className="Title">
+                <h1>Welcome to </h1>
+                <img src={Logo} alt="logo"/>
+                <h1> Guide page</h1>
+            </div>
+
+        </div>
     )   
 }
 

8aaffbc05278d686b3b55e5a2e20191e27d807b6
BaEsp1 <97913060+BaEsp1@users.noreply.github.com>
2023-09-19 23:07:56 -0300

    Design Home

--- Front/src/Views/Home.js
+++ Front/src/Views/Home.js
@@ -30,30 +30,34 @@ function Home () {
 
             <Link className="HowPlay">
                 <div className="ImgHome">
-                    <h1>How play?</h1>
-                    <p>eaioeadioaejuiodjioqwjedaiodjioaejidoa </p>
+                    <h1>How to play?</h1>
+                    <p>League of Legends is a team-based strategy game where two teams of five powerful champions face off to destroy the other’s base. Choose from over 140 champions to make epic plays, secure kills, and take down towers as you battle your way to victory.</p>
                 </div>
             </Link>
             <div className="Order">
 
                 <div className="Columna1">
-                    <Link to="" >Support</Link>                
+                    <Link to="" >SUPPORT</Link>             
+                    <p>Carousel</p>   
                     <Link to="" >ADC</Link>
+                    <p>Carousel</p>  
                 </div>
 
                 <div className="Carousel2">
                 <MyCarousel2/>
+                <br></br>
+                <Link to="" >Mid Line</Link>
+                <p>Carousel</p>  
                 </div>
 
-                <div className="Columna2">
-                    <Link to="" >Mid Line</Link>
-                </div>
-
-                <div className="Columna3"></div>
-                    <Link to="" >Top Line</Link>                
+                <div className="Columna3">
+                    <Link to="" >Top Line</Link>   
+                    <p>Carousel</p>               
                     <Link to="" >Jungle</Link>
+                    <p>Carousel</p>  
                 </div>
-            
+
+                </div>
             </div>
         </div>
     )   

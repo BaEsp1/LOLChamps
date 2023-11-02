b7492276266f8762382a2ca3a938675749619c40
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-02 20:54:32 -0300

    update home
    changes png & css

--- Front/src/Components/Carrousel/Home-Maps.jsx
+++ Front/src/Components/Carrousel/Home-Maps.jsx
@@ -11,7 +11,7 @@ import 'slick-carousel/slick/slick-theme.css';
 
 const MyCarousel2 = () => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1, 
--- Front/src/Views/Home.js
+++ Front/src/Views/Home.js
@@ -3,46 +3,45 @@ import { Link } from 'react-router-dom';
 import "./Home.css" ;
 import MyCarousel2 from "../Components/Carrousel/Home-Maps";
 import {TanksC, MagesC , AssasC, SuppC, MarkC, FigthC} from "../Components/Carrousel/Champs";
-import BG from "../lol/Banner2.png"
 
 
 function Home() {
 
-    const fondo = BG;
+    // const fondo = BG;
 
   return (
     <div className="Home">
-            <Link to="/howToPlay" className="HowPlay">
-                <div>            
-                    <img src={fondo} className="ImgHome" alt="BG" ></img>
-                    <h1>How to play?</h1>
-                    <p>League of Legends is a team-based strategy game where two teams of five powerful champions face off to destroy the other’s base. Choose from over 140 champions to make epic plays, secure kills, and take down towers as you battle your way to victory.</p>
-                </div>
-            </Link>
+
 
         <div className="Order">
             <div className="Columna1">
-                <div>
+                <div className="back">
                     <h2>SUPPORT</h2>
                     <SuppC/>                
                 </div>
 
-                <div>
+                <div className="back">
                     <h2>ASSASSIN</h2>
                     <AssasC/>                
                 </div>
             </div>
 
             <div className="Carousel2">
+                <div className="HwP">            
+                     <Link to="/howToPlay" >
+                    <h1>How to play?</h1>
+                    <p>League of Legends is a team-based strategy game where two teams of five powerful champions face off to destroy the other’s base.</p>
+                    </Link>
+                </div>
                 <MyCarousel2 />
                 <br></br>
                 <div className="botombutons">
-                    <div>
+                    <div className="back">
                         <h2>TANK</h2>
                         <TanksC/>
                     </div>
                     <br></br>
-                    <div>
+                    <div className="back">
                         <h2>FIGTHER</h2>
                         <FigthC/>
                     </div>
@@ -51,11 +50,11 @@ function Home() {
             </div>
 
             <div className="Columna3">
-                <div>
+                <div className="back">
                     <h2>MAGE</h2>
                     < MagesC/>
                 </div>
-                <div>
+                <div className="back">
                     <h2>MARKSMAN</h2>
                     <MarkC/>
                 </div>

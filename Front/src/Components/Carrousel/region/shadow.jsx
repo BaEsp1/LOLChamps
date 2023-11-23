59aba42b136a2808fb82a4ed710667afbb4ee46c
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-23 18:45:27 -0300

    map carr region 08/11

--- Front/src/Components/Carrousel/region/shadow.jsx
+++ Front/src/Components/Carrousel/region/shadow.jsx
@@ -1,1 +1,97 @@
-//elise ,Gwen , Hecarin , Kalista , Karthus , maokai , Treesh , vex , viego y yorick
+//elise ,Gwen , Hecarim , Kalista , Karthus , maokai , Thresh , vex , viego y yorick
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Elise from "../../../lol/square/Elise.png";
+import Gwen from "../../../lol/square/Gwen.png";
+import Hecarim from "../../../lol/square/Hecarim.png";
+import Kalista from "../../../lol/square/Kalista.png";
+import Karthus from "../../../lol/square/Karthus.png";
+import Maokai from "../../../lol/square/Maokai.png";
+import Thresh from "../../../lol/square/Thresh.png";
+import Vex from "../../../lol/square/Vex.png";
+import Viego from "../../../lol/square/Viego.png";
+import Yorick from "../../../lol/square/Yorick.png";
+import { useDispatch } from 'react-redux';
+import { getDetail } from '../../Redux/Actions';
+import { Link } from 'react-router-dom';
+
+const Shadow = () => {
+
+    const dispatch = useDispatch();
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5, 
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        arrows: true,
+    };
+
+
+   const handleDetailClick = (id) => {
+    dispatch(getDetail(id));
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <Link to={`/detail/${encodeURI("Elise")}`} onClick={() => handleDetailClick("Elise")}>
+        <img src={Elise} alt="Imagen 1" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Gwen")}`} onClick={() => handleDetailClick("Gwen")}>
+        <img src={Gwen} alt="Imagen 2" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Hecarim")}`} onClick={() => handleDetailClick("Hecarim")}>
+        <img src={Hecarim} alt="Imagen 3" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Kalista")}`} onClick={() => handleDetailClick("Kalista")}>
+        <img src={Kalista} alt="Imagen 4" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Karthus")}`} onClick={() => handleDetailClick("Karthus")}>
+        <img src={Karthus} alt="Imagen 5" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Maokai")}`} onClick={() => handleDetailClick("Maokai")}>
+        <img src={Maokai} alt="Imagen 6" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Thresh")}`} onClick={() => handleDetailClick("Thresh")}>
+        <img src={Thresh} alt="Imagen 7" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Vex")}`} onClick={() => handleDetailClick("Vex")}>
+        <img src={Vex} alt="Imagen 8"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Viego")}`} onClick={() => handleDetailClick("Viego")}>
+        <img src={Viego} alt="Imagen 9"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Yorick")}`} onClick={() => handleDetailClick("Yorick")}>
+        <img src={Yorick} alt="Imagen 10"/>
+        </Link>
+      </div>
+    </Slider>
+  );
+};
+
+export default Shadow;
--- Front/src/Views/Maps.js
+++ Front/src/Views/Maps.js
@@ -52,6 +52,7 @@ import Ionia from "../Components/Carrousel/region/ionia";
 import Ixtal from "../Components/Carrousel/region/ixtal";
 import Noxus from "../Components/Carrousel/region/noxus";
 import Piltover from "../Components/Carrousel/region/piltover";
+import Shadow from "../Components/Carrousel/region/shadow";
 
 function Maps() {
   const [informacion, setInformacion] = useState(""); // Estado para almacenar la información
@@ -119,7 +120,7 @@ function ocultarDivYBotones() {
         imagenes: [Sha1, Sha2 , Sha3],
         titulo: "SHADOW ISLES",
         subtitulo: "LANDS SHROUDED BY THE BLACK MIST",
-        carr:"carrousel de Shadow",
+        carr:<Shadow/>,
       },
     Ionia:{
         texto: "Surrounded by treacherous seas, Ionia is composed of a number of allied provinces scattered across a massive archipelago, known to many as the First Lands. Since Ionian culture has long been shaped by the pursuit of balance in all things, the border between the material and spirit realms tends to be more permeable here, especially in the wild forests and mountains. Although these lands’ enchantments can be fickle, its creatures dangerous and fae, for many centuries most Ionians led lives of plenty. The warrior monasteries, provincial militias—and even Ionia itself—had been enough to protect them. But that ended twelve years ago, when Noxus attacked the First Lands. The empire’s seemingly endless warhosts savaged Ionia, and were only defeated after many years, and at great cost. Now, Ionia exists in an uneasy peace. Different reactions to the war have divided the region—some groups, such as the Shojin monks or the Kinkou, seek a return to isolationist pacifism, and pastoral traditions. Other more radical factions, such as the Navori Brotherhood and the Order of Shadow, demand a militarization of the land’s magic, to create a single, unified nation that can take vengeance on Noxus. The fate of Ionia hangs in a delicate balance that few are willing to overturn, but all can feel shifting uneasily beneath their feet.",

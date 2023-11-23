c6389dc5a52c378e851f1673522c93c04f19279c
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-23 18:03:28 -0300

    region map 4/11

--- Front/src/Components/Carrousel/region/ionia.jsx
+++ Front/src/Components/Carrousel/region/ionia.jsx
@@ -1,2 +1,164 @@
 
 //Ahri , Akali, Irelia , Ivern , Jhin , karma , kayn , Kennen , Lee Sin , Lillia , Master Yi , Rakan , sett, Shen , Syndra, Varus , wukong, Xayah, Yasuo,yone , Zed
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Ahri from "../../../lol/square/Ahri.png";
+import Akali from "../../../lol/square/Akali.png";
+import Irelia from "../../../lol/square/Irelia.png";
+import Ivern from "../../../lol/square/Ivern.png";
+import Jhin from "../../../lol/square/Jhin.png";
+import Karma from "../../../lol/square/Karma.png";
+import Kayn from "../../../lol/square/Kayn.png";
+import Kennen from "../../../lol/square/Kennen.png";
+import LeeSin from "../../../lol/square/LeeSin.png";
+import Lillia from "../../../lol/square/Lillia.png";
+import MasterYi from "../../../lol/square/MasterYi.png";
+import Rakan from "../../../lol/square/Rakan.png";
+import Sett from "../../../lol/square/Sett.png";
+import Shen from "../../../lol/square/Shen.png";
+import Syndra from "../../../lol/square/Syndra.png";
+import Varus from "../../../lol/square/Varus.png";
+import Wukong from "../../../lol/square/MonkeyKing.png";
+import Xayah from "../../../lol/square/Xayah.png";
+import Yasuo from "../../../lol/square/Yasuo.png";
+import Yone from "../../../lol/square/Yone.png";
+import Zed from "../../../lol/square/Zed.png";
+import { useDispatch } from 'react-redux';
+import { getDetail } from '../../Redux/Actions';
+import { Link } from 'react-router-dom';
+
+const Ionia = () => {
+
+    const dispatch = useDispatch();
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5, 
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        arrows: true,
+    };
+
+
+   const handleDetailClick = (id) => {
+    dispatch(getDetail(id));
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <Link to={`/detail/${encodeURI("Ahri")}`} onClick={() => handleDetailClick("Ahri")}>
+        <img src={Ahri} alt="Imagen 1" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Akali")}`} onClick={() => handleDetailClick("Akali")}>
+        <img src={Akali} alt="Imagen 2" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Irelia")}`} onClick={() => handleDetailClick("Irelia")}>
+        <img src={Irelia} alt="Imagen 3" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Ivern")}`} onClick={() => handleDetailClick("Ivern")}>
+        <img src={Ivern} alt="Imagen 4" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Jhin")}`} onClick={() => handleDetailClick("Jhin")}>
+        <img src={Jhin} alt="Imagen 5" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Karma")}`} onClick={() => handleDetailClick("Karma")}>
+        <img src={Karma} alt="Imagen 6" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Kayn")}`} onClick={() => handleDetailClick("Kayn")}>
+        <img src={Kayn} alt="Imagen 7" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Kennen")}`} onClick={() => handleDetailClick("Kennen")}>
+        <img src={Kennen} alt="Imagen 8"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("LeeSin")}`} onClick={() => handleDetailClick("LeeSin")}>
+        <img src={LeeSin} alt="Imagen 9"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Lillia")}`} onClick={() => handleDetailClick("Lillia")}>
+        <img src={Lillia} alt="Imagen 10"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("MasterYi")}`} onClick={() => handleDetailClick("MasterYi")}>
+        <img src={MasterYi} alt="Imagen 11"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Rakan")}`} onClick={() => handleDetailClick("Rakan")}>
+        <img src={Rakan} alt="Imagen 12"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Sett")}`} onClick={() => handleDetailClick("Sett")}>
+        <img src={Sett} alt="Imagen 13"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Shen")}`} onClick={() => handleDetailClick("Shen")}>
+        <img src={Shen} alt="Imagen 14"/>
+        </Link>
+      </div>
+        <div>
+      <Link to={`/detail/${encodeURI("Syndra")}`} onClick={() => handleDetailClick("Syndra")}>
+        <img src={Syndra} alt="Imagen 15"/>
+        </Link>
+      </div>    
+        <div>
+      <Link to={`/detail/${encodeURI("Varus")}`} onClick={() => handleDetailClick("Varus")}>
+        <img src={Varus} alt="Imagen 16"/>
+        </Link>
+      </div>  
+        <div>
+      <Link to={`/detail/${encodeURI("MonkeyKing")}`} onClick={() => handleDetailClick("MonkeyKing")}>
+        <img src={Wukong} alt="Imagen 17"/>
+        </Link>
+      </div>  
+        <div>
+      <Link to={`/detail/${encodeURI("Xayah")}`} onClick={() => handleDetailClick("Xayah")}>
+        <img src={Xayah} alt="Imagen 18"/>
+        </Link>
+      </div>  
+        <div>
+      <Link to={`/detail/${encodeURI("Yasuo")}`} onClick={() => handleDetailClick("Yasuo")}>
+        <img src={Yasuo} alt="Imagen 19"/>
+        </Link>
+      </div>  
+        <div>
+      <Link to={`/detail/${encodeURI("Yone")}`} onClick={() => handleDetailClick("Yone")}>
+        <img src={Yone} alt="Imagen 20"/>
+        </Link>
+      </div>  
+           <div>
+      <Link to={`/detail/${encodeURI("Zed")}`} onClick={() => handleDetailClick("Zed")}>
+        <img src={Zed} alt="Imagen 21"/>
+        </Link>
+      </div>     
+    </Slider>
+  );
+};
+
+export default Ionia;
--- Front/src/Views/Maps.js
+++ Front/src/Views/Maps.js
@@ -48,6 +48,7 @@ import Footer from "../Components/Footer";
 import Bilgewater from "../Components/Carrousel/region/bilgewater";
 import Demacia from "../Components/Carrousel/region/demacia";
 import Freljord from "../Components/Carrousel/region/freljord";
+import Ionia from "../Components/Carrousel/region/ionia";
 
 function Maps() {
   const [informacion, setInformacion] = useState(""); // Estado para almacenar la información
@@ -122,7 +123,7 @@ function ocultarDivYBotones() {
         imagenes: [Io1, Io2,Io3],
         titulo: "Ionia",
         subtitulo: "THE FIRST LANDS",
-        carr:"carrousel de Ionia",
+        carr:<Ionia/>,
       },
     Piltover: {
         texto: "Piltover is a thriving, progressive city whose power and influence is on the rise. It is Valoran’s cultural center, where art, craftsmanship, trade and innovation walk hand in hand. Its power comes not through military might, but the engines of commerce and forward thinking. Situated on the cliffs above the district of Zaun and overlooking the ocean, fleets of ships pass through its titanic sea-gates, bringing goods from all over the world. The wealth this generates has given rise to an unprecedented boom in the city’s growth. Piltover has - and still is - reinventing itself as a city where fortunes can be made and dreams can be lived. Burgeoning merchant clans fund development in the most incredible endeavors: grand artistic follies, esoteric hextech research, and architectural monuments to their power. With ever more inventors delving into the emergent lore of hextech, Piltover has become a lodestone for the most skilled craftsmen the world over.",

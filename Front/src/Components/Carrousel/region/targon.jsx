a46af83909b3a767a3968f54c9228eb55bbf929f
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-23 19:07:21 -0300

    edit

--- Front/src/Components/Carrousel/region/targon.jsx
+++ Front/src/Components/Carrousel/region/targon.jsx
@@ -33,52 +33,52 @@ const Targon = () => {
     };
 
 
-   const handleDetailClick = (id) => {
+    const handleDetailClick = (id) => {
     dispatch(getDetail(id));
-  };
+    };
 
-  return (
+    return (
     <Slider {...settings}>
-      <div>
-        <Link to={`/detail/${encodeURI("Aphelios")}`} onClick={() => handleDetailClick("Aphelios")}>
-        <img src={Aphelios} alt="Imagen 1" />
-        </Link>
-      </div>
-      <div>
-      <Link to={`/detail/${encodeURI("AurelionSol")}`} onClick={() => handleDetailClick("AurelionSol")}>
-        <img src={AurelionSol} alt="Imagen 2" />
-        </Link>
-      </div>
-      <div>
-        <Link to={`/detail/${encodeURI("Diana")}`} onClick={() => handleDetailClick("Diana")}>
-        <img src={Diana} alt="Imagen 3" />
-        </Link>
-      </div>
-      <div>
-      <Link to={`/detail/${encodeURI("Leona")}`} onClick={() => handleDetailClick("Leona")}>
-        <img src={Leona} alt="Imagen 4" />
-        </Link>
-      </div>
-      <div>
-      <Link to={`/detail/${encodeURI("Pantheon")}`} onClick={() => handleDetailClick("Pantheon")}>
-        <img src={Pantheon} alt="Imagen 5" />
-        </Link>
-      </div>
-      <div>
-        <Link to={`/detail/${encodeURI("Soraka ")}`} onClick={() => handleDetailClick("Soraka ")}>
-        <img src={Soraka } alt="Imagen 6" />
-        </Link>
-      </div>
-      <div>
-      <Link to={`/detail/${encodeURI("Taric")}`} onClick={() => handleDetailClick("Taric")}>
-        <img src={Taric} alt="Imagen 7" />
-        </Link>
-      </div>
-      <div>
-      <Link to={`/detail/${encodeURI("Zoe")}`} onClick={() => handleDetailClick("Zoe")}>
-        <img src={Zoe} alt="Imagen 8"/>
-        </Link>
-      </div>
+        <div>
+            <Link to={`/detail/${encodeURI("Aphelios")}`} onClick={() => handleDetailClick("Aphelios")}>
+            <img src={Aphelios} alt="Imagen 1" />
+            </Link>
+        </div>
+        <div>
+        <Link to={`/detail/${encodeURI("AurelionSol")}`} onClick={() => handleDetailClick("AurelionSol")}>
+            <img src={AurelionSol} alt="Imagen 2" />
+            </Link>
+        </div>
+        <div>
+            <Link to={`/detail/${encodeURI("Diana")}`} onClick={() => handleDetailClick("Diana")}>
+            <img src={Diana} alt="Imagen 3" />
+            </Link>
+        </div>
+        <div>
+        <Link to={`/detail/${encodeURI("Leona")}`} onClick={() => handleDetailClick("Leona")}>
+            <img src={Leona} alt="Imagen 4" />
+            </Link>
+        </div>
+        <div>
+        <Link to={`/detail/${encodeURI("Pantheon")}`} onClick={() => handleDetailClick("Pantheon")}>
+            <img src={Pantheon} alt="Imagen 5" />
+            </Link>
+        </div>
+        <div>
+            <Link to={`/detail/${encodeURI("Soraka ")}`} onClick={() => handleDetailClick("Soraka ")}>
+            <img src={Soraka } alt="Imagen 6" />
+            </Link>
+        </div>
+        <div>
+        <Link to={`/detail/${encodeURI("Taric")}`} onClick={() => handleDetailClick("Taric")}>
+            <img src={Taric} alt="Imagen 7" />
+            </Link>
+        </div>
+        <div>
+        <Link to={`/detail/${encodeURI("Zoe")}`} onClick={() => handleDetailClick("Zoe")}>
+            <img src={Zoe} alt="Imagen 8"/>
+            </Link>
+        </div>
     </Slider>
   );
 };

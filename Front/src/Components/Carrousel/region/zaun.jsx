9d0d62306aeb0100692bdb24353eebdda9842d0b
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-23 19:04:44 -0300

    carr map full ON

--- Front/src/Components/Carrousel/region/zaun.jsx
+++ Front/src/Components/Carrousel/region/zaun.jsx
@@ -1,2 +1,122 @@
 
-// Blitzcrank , DrMundo , ekko , janna , jinx , renata , singed, twitch , urgot, viktor, warwick , zac , ziggs , zeri
+// Blitzcrank, DrMundo , Ekko, Janna , Jinx, Renata ,Singed , Twitch, Urgot , viktor ,Warwick , Zac , Ziggs y Zeri
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Blitzcrank from "../../../lol/square/Blitzcrank.png";
+import DrMundo from "../../../lol/square/DrMundo.png";
+import Ekko from "../../../lol/square/Ekko.png";
+import Janna from "../../../lol/square/Janna.png";
+import Jinx from "../../../lol/square/Jinx.png";
+import Renata from "../../../lol/square/Renata.png";
+import Singed from "../../../lol/square/Singed.png";
+import Twitch from "../../../lol/square/Twitch.png";
+import Urgot from "../../../lol/square/Urgot.png";
+import Viktor from "../../../lol/square/Viktor.png";
+import Warwick from "../../../lol/square/Warwick.png";
+import Zac from "../../../lol/square/Zac.png";
+import Ziggs from "../../../lol/square/Ziggs.png";
+import Zeri from "../../../lol/square/Zeri.png";
+import { useDispatch } from 'react-redux';
+import { getDetail } from '../../Redux/Actions';
+import { Link } from 'react-router-dom';
+
+const Zaun = () => {
+
+    const dispatch = useDispatch();
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5, 
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        arrows: true,
+    };
+
+
+   const handleDetailClick = (id) => {
+    dispatch(getDetail(id));
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <Link to={`/detail/${encodeURI("Blitzcrank")}`} onClick={() => handleDetailClick("Blitzcrank")}>
+        <img src={Blitzcrank} alt="Imagen 1" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("DrMundo")}`} onClick={() => handleDetailClick("DrMundo")}>
+        <img src={DrMundo} alt="Imagen 2" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Ekko")}`} onClick={() => handleDetailClick("Ekko")}>
+        <img src={Ekko} alt="Imagen 3" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Janna")}`} onClick={() => handleDetailClick("Janna")}>
+        <img src={Janna} alt="Imagen 4" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Jinx")}`} onClick={() => handleDetailClick("Jinx")}>
+        <img src={Jinx} alt="Imagen 5" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Renata")}`} onClick={() => handleDetailClick("Renata")}>
+        <img src={Renata} alt="Imagen 6" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Singed")}`} onClick={() => handleDetailClick("Singed")}>
+        <img src={Singed} alt="Imagen 7" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Twitch")}`} onClick={() => handleDetailClick("Twitch")}>
+        <img src={Twitch} alt="Imagen 8"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Urgot")}`} onClick={() => handleDetailClick("Urgot")}>
+        <img src={Urgot} alt="Imagen 9"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Viktor")}`} onClick={() => handleDetailClick("Viktor")}>
+        <img src={Viktor} alt="Imagen 10"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Warwick")}`} onClick={() => handleDetailClick("Warwick")}>
+        <img src={Warwick} alt="Imagen 11"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Zac")}`} onClick={() => handleDetailClick("Zac")}>
+        <img src={Zac} alt="Imagen 12"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Ziggs")}`} onClick={() => handleDetailClick("Ziggs")}>
+        <img src={Ziggs} alt="Imagen 13"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Zeri")}`} onClick={() => handleDetailClick("Zeri")}>
+        <img src={Zeri} alt="Imagen 14"/>
+        </Link>
+      </div>   
+    </Slider>
+  );
+};
+
+export default Zaun;
--- Front/src/Views/Maps.js
+++ Front/src/Views/Maps.js
@@ -55,6 +55,7 @@ import Piltover from "../Components/Carrousel/region/piltover";
 import Shadow from "../Components/Carrousel/region/shadow";
 import Shurima from "../Components/Carrousel/region/shurima";
 import Targon from "../Components/Carrousel/region/targon";
+import Zaun from "../Components/Carrousel/region/zaun";
 
 function Maps() {
   const [informacion, setInformacion] = useState(""); // Estado para almacenar la información
@@ -143,7 +144,7 @@ function ocultarDivYBotones() {
         imagenes: [ Za1 , Za2, Za3],
         titulo: "ZAUN",
         subtitulo: "POLLUTED UNDERCITY",
-        carr:"carrousel de Zaun",
+        carr:<Zaun/>,
       },
     Bilgewater: {
         texto: "Nestled away in the Blue Flame Isles archipelago, Bilgewater is a port city like no other—home to serpent hunters, dock gangs, and smugglers from across the known world. Here, fortunes are made and ambitions shattered in the blink of an eye. For those fleeing justice, debt, or persecution, Bilgewater can be a place of new beginnings, for no one on these twisted streets cares about your past. Even so, with each new dawn, careless travelers can always be found floating in the harbor, their purses empty and their throats slit… While incredibly dangerous, Bilgewater is ripe with opportunity, free from the shackles of formal government and trade regulation. If you have the coin, almost anything can be purchased here, from outlawed hextech to the favor of local crime lords. With the recent removal of the last “reaver king” of Bilgewater, the city has entered a period of transition, while the most prominent captains try to agree on its future. But as long as there are seaworthy ships and crews to sail them, Bilgewater is likely to remain one of the most colorful and well-connected places in Runeterra.",

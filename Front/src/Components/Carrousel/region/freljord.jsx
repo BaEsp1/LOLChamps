716ba816752daf501c9ad1d9bf3237044dd6496d
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-23 17:47:30 -0300

    map 03/11

--- Front/src/Components/Carrousel/region/freljord.jsx
+++ Front/src/Components/Carrousel/region/freljord.jsx
@@ -1,2 +1,122 @@
 
-// Anivia, Ashe , Braum, Gnar , Gragas, Lissandra , Nunu & Willump , Olaf, Ornn , Sejuana , trundle , tryndamere , Udyr y Volibear
+// Anivia, Ashe , Braum, Gnar , Gragas, Lissandra , Nunu , Olaf, Ornn , Sejuana , trundle , tryndamere , Udyr y Volibear
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Anivia from "../../../lol/square/Anivia.png";
+import Ashe from "../../../lol/square/Ashe.png";
+import Braum from "../../../lol/square/Braum.png";
+import Gnar from "../../../lol/square/Gnar.png";
+import Gragas from "../../../lol/square/Gragas.png";
+import Lissandra from "../../../lol/square/Lissandra.png";
+import Nunu from "../../../lol/square/Nunu.png";
+import Olaf from "../../../lol/square/Olaf.png";
+import Ornn from "../../../lol/square/Ornn.png";
+import Sejuani from "../../../lol/square/Sejuani.png";
+import Trundle from "../../../lol/square/Trundle.png";
+import Tryndamere from "../../../lol/square/Tryndamere.png";
+import Udyr from "../../../lol/square/Udyr.png";
+import Volibear from "../../../lol/square/Volibear.png";
+import { useDispatch } from 'react-redux';
+import { getDetail } from '../../Redux/Actions';
+import { Link } from 'react-router-dom';
+
+const Freljord = () => {
+
+    const dispatch = useDispatch();
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5, 
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        arrows: true,
+    };
+
+
+   const handleDetailClick = (id) => {
+    dispatch(getDetail(id));
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <Link to={`/detail/${encodeURI("Anivia")}`} onClick={() => handleDetailClick("Anivia")}>
+        <img src={Anivia} alt="Imagen 1" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Ashe")}`} onClick={() => handleDetailClick("Ashe")}>
+        <img src={Ashe} alt="Imagen 2" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Braum")}`} onClick={() => handleDetailClick("Braum")}>
+        <img src={Braum} alt="Imagen 3" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Gnar")}`} onClick={() => handleDetailClick("Gnar")}>
+        <img src={Gnar} alt="Imagen 4" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Gragas")}`} onClick={() => handleDetailClick("Gragas")}>
+        <img src={Gragas} alt="Imagen 5" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Lissandra")}`} onClick={() => handleDetailClick("Lissandra")}>
+        <img src={Lissandra} alt="Imagen 6" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Nunu")}`} onClick={() => handleDetailClick("Nunu")}>
+        <img src={Nunu} alt="Imagen 7" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Olaf")}`} onClick={() => handleDetailClick("Olaf")}>
+        <img src={Olaf} alt="Imagen 8"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Ornn")}`} onClick={() => handleDetailClick("Ornn")}>
+        <img src={Ornn} alt="Imagen 9"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Sejuani")}`} onClick={() => handleDetailClick("Sejuani")}>
+        <img src={Sejuani} alt="Imagen 10"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Trundle")}`} onClick={() => handleDetailClick("Trundle")}>
+        <img src={Trundle} alt="Imagen 11"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Tryndamere")}`} onClick={() => handleDetailClick("Tryndamere")}>
+        <img src={Tryndamere} alt="Imagen 12"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Udyr")}`} onClick={() => handleDetailClick("Udyr")}>
+        <img src={Udyr} alt="Imagen 13"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Volibear")}`} onClick={() => handleDetailClick("Volibear")}>
+        <img src={Volibear} alt="Imagen 14"/>
+        </Link>
+      </div>   
+    </Slider>
+  );
+};
+
+export default Freljord;
--- Front/src/Views/Maps.js
+++ Front/src/Views/Maps.js
@@ -47,6 +47,7 @@ import ZaunS from "../lol/Cities/Rv/ZaunS.png";
 import Footer from "../Components/Footer";
 import Bilgewater from "../Components/Carrousel/region/bilgewater";
 import Demacia from "../Components/Carrousel/region/demacia";
+import Freljord from "../Components/Carrousel/region/freljord";
 
 function Maps() {
   const [informacion, setInformacion] = useState(""); // Estado para almacenar la información
@@ -79,7 +80,7 @@ function ocultarDivYBotones() {
       imagenes: [ Frel2, Frel1, Frel3],
       titulo: "FRELJORD",
       subtitulo: "HARSH FROZEN LAND",
-      carr:"carrousel de freljord",
+      carr:<Freljord/>,
     },
     Demacia: {
       texto: "Astrong, lawful kingdom with a prestigious military history, Demacia’s people have always valued the ideals of justice, honor, and duty most highly, and are fiercely proud of their cultural heritage. But in spite of these lofty principles, this largely self-sufficient nation has grown more insular and isolationist in recent centuries. Now, Demacia is a kingdom in turmoil. The capital, the Great City of Demacia, was founded as a refuge from sorcery after the nightmare of the Rune Wars, and built upon the riddle of petricite—a peculiar white stone that dampens magical energy. It is from here that the royal family has long seen to the defense of the outlying towns and villages, farmland, forests, and mountains rich with mineral resources. However, following the sudden death of King Jarvan III, the other noble families have not yet approved the succession of his sole heir, young Prince Jarvan, to the throne. Those who dwell beyond the heavily guarded borders are increasingly viewed with suspicion, and many former allies have begun to look elsewhere for protection, in these uncertain times. Some dare to whisper that the golden age of Demacia has passed, and unless its people are willing to adapt to a changing world—something many believe they are simply incapable of doing—the kingdom’s decline may be inevitable. And all the petricite in the land will not protect Demacia from itself.",

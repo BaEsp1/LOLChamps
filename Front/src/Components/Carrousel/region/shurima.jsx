9681ce2dcfb003a1e1e4f2c22b8323c20262b080
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-23 18:49:39 -0300

    map carru region 09/11

--- Front/src/Components/Carrousel/region/shurima.jsx
+++ Front/src/Components/Carrousel/region/shurima.jsx
@@ -1,2 +1,98 @@
 
-//Akshan , amumu , azir , nasus , rammus , renekton , sivir , skarner, taliyah , xerath
+//Akshan ,Amumu , Azir , Nasus , Rammus , Renekton , Sivir , Skarner , Taliyah y Xerath
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Akshan from "../../../lol/square/Akshan.png";
+import Amumu from "../../../lol/square/Amumu.png";
+import Azir from "../../../lol/square/Azir.png";
+import Nasus from "../../../lol/square/Nasus.png";
+import Rammus from "../../../lol/square/Rammus.png";
+import Renekton from "../../../lol/square/Renekton.png";
+import Sivir from "../../../lol/square/Sivir.png";
+import Skarner from "../../../lol/square/Skarner.png";
+import Taliyah from "../../../lol/square/Taliyah.png";
+import Xerath from "../../../lol/square/Xerath.png";
+import { useDispatch } from 'react-redux';
+import { getDetail } from '../../Redux/Actions';
+import { Link } from 'react-router-dom';
+
+const Shurima = () => {
+
+    const dispatch = useDispatch();
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5, 
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        arrows: true,
+    };
+
+
+   const handleDetailClick = (id) => {
+    dispatch(getDetail(id));
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <Link to={`/detail/${encodeURI("Akshan")}`} onClick={() => handleDetailClick("Akshan")}>
+        <img src={Akshan} alt="Imagen 1" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Amumu")}`} onClick={() => handleDetailClick("Amumu")}>
+        <img src={Amumu} alt="Imagen 2" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Azir")}`} onClick={() => handleDetailClick("Azir")}>
+        <img src={Azir} alt="Imagen 3" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Nasus")}`} onClick={() => handleDetailClick("Nasus")}>
+        <img src={Nasus} alt="Imagen 4" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Rammus")}`} onClick={() => handleDetailClick("Rammus")}>
+        <img src={Rammus} alt="Imagen 5" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Renekton")}`} onClick={() => handleDetailClick("Renekton")}>
+        <img src={Renekton} alt="Imagen 6" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Sivir")}`} onClick={() => handleDetailClick("Sivir")}>
+        <img src={Sivir} alt="Imagen 7" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Skarner")}`} onClick={() => handleDetailClick("Skarner")}>
+        <img src={Skarner} alt="Imagen 8"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Taliyah")}`} onClick={() => handleDetailClick("Taliyah")}>
+        <img src={Taliyah} alt="Imagen 9"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Xerath")}`} onClick={() => handleDetailClick("Xerath")}>
+        <img src={Xerath} alt="Imagen 10"/>
+        </Link>
+      </div>
+    </Slider>
+  );
+};
+
+export default Shurima;
--- Front/src/Views/Maps.js
+++ Front/src/Views/Maps.js
@@ -53,6 +53,7 @@ import Ixtal from "../Components/Carrousel/region/ixtal";
 import Noxus from "../Components/Carrousel/region/noxus";
 import Piltover from "../Components/Carrousel/region/piltover";
 import Shadow from "../Components/Carrousel/region/shadow";
+import Shurima from "../Components/Carrousel/region/shurima";
 
 function Maps() {
   const [informacion, setInformacion] = useState(""); // Estado para almacenar la información
@@ -113,7 +114,7 @@ function ocultarDivYBotones() {
         imagenes: [Shur1, Shur2, Shur3],
         titulo: "SHURIMA",
         subtitulo: "FALLEN DESERT EMPIRE",
-        carr:"carrousel de Shurima",
+        carr:<Shurima/>,
       },
     Shadow: {
         texto: "This cursed land was once home to a noble, enlightened civilization, known to its allies and emissaries as the Blessed Isles. However, more than a thousand years ago, an unprecedented magical cataclysm left the barrier between the material and spirit realms in tatters, effectively merging the two… and dooming all living things in an instant. Now, a malevolent Black Mist permanently shrouds the Isles, and the earth itself is tainted by dark sorcery. Mortals who dare to venture to these dismal shores will slowly have their life force stolen away from them, which in turn attracts the insatiable, restless spirits of the dead. Those who perish within the Mist are condemned to haunt this nightmarish place for eternity—worse still, the power of the Shadow Isles appears to wax stronger with every passing year, allowing the most powerful specters to roam farther and farther across Runeterra.",

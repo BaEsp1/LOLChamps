415e713fb4040f9b4a31f0b650df784c864b6f47
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-23 18:30:46 -0300

    carru map region 6/11

--- Front/src/Components/Carrousel/region/noxus.jsx
+++ Front/src/Components/Carrousel/region/noxus.jsx
@@ -1,2 +1,125 @@
 
 //Cassiopeia , Darius ,Draven , Katarina, Kled , Leblanc, Mordekaiser , Rell , Riven , Samira , Sion , Swain , talon , vladimir
+
+
+// Cassiopeia, Darius , Draven, Katarina , Kled, Leblanc , Mordekaiser , Rell, Riven, Samira , Sion , Swain , Talon y Vladimir
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Cassiopeia from "../../../lol/square/Cassiopeia.png";
+import Darius from "../../../lol/square/Darius.png";
+import Draven from "../../../lol/square/Draven.png";
+import Katarina from "../../../lol/square/Katarina.png";
+import Kled from "../../../lol/square/Kled.png";
+import Leblanc from "../../../lol/square/Leblanc.png";
+import Mordekaiser from "../../../lol/square/Mordekaiser.png";
+import Rell from "../../../lol/square/Rell.png";
+import Riven from "../../../lol/square/Riven.png";
+import Samira from "../../../lol/square/Samira.png";
+import Sion from "../../../lol/square/Sion.png";
+import Swain from "../../../lol/square/Swain.png";
+import Talon from "../../../lol/square/Talon.png";
+import Vladimir from "../../../lol/square/Vladimir.png";
+import { useDispatch } from 'react-redux';
+import { getDetail } from '../../Redux/Actions';
+import { Link } from 'react-router-dom';
+
+const Noxus = () => {
+
+    const dispatch = useDispatch();
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5, 
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1800,
+        arrows: true,
+    };
+
+
+   const handleDetailClick = (id) => {
+    dispatch(getDetail(id));
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <Link to={`/detail/${encodeURI("Cassiopeia")}`} onClick={() => handleDetailClick("Cassiopeia")}>
+        <img src={Cassiopeia} alt="Imagen 1" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Darius")}`} onClick={() => handleDetailClick("Darius")}>
+        <img src={Darius} alt="Imagen 2" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Draven")}`} onClick={() => handleDetailClick("Draven")}>
+        <img src={Draven} alt="Imagen 3" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Katarina")}`} onClick={() => handleDetailClick("Katarina")}>
+        <img src={Katarina} alt="Imagen 4" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Kled")}`} onClick={() => handleDetailClick("Kled")}>
+        <img src={Kled} alt="Imagen 5" />
+        </Link>
+      </div>
+      <div>
+        <Link to={`/detail/${encodeURI("Leblanc")}`} onClick={() => handleDetailClick("Leblanc")}>
+        <img src={Leblanc} alt="Imagen 6" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Mordekaiser")}`} onClick={() => handleDetailClick("Mordekaiser")}>
+        <img src={Mordekaiser} alt="Imagen 7" />
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Rell")}`} onClick={() => handleDetailClick("Rell")}>
+        <img src={Rell} alt="Imagen 8"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Riven")}`} onClick={() => handleDetailClick("Riven")}>
+        <img src={Riven} alt="Imagen 9"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Samira")}`} onClick={() => handleDetailClick("Samira")}>
+        <img src={Samira} alt="Imagen 10"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Sion")}`} onClick={() => handleDetailClick("Sion")}>
+        <img src={Sion} alt="Imagen 11"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Swain")}`} onClick={() => handleDetailClick("Swain")}>
+        <img src={Swain} alt="Imagen 12"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Talon")}`} onClick={() => handleDetailClick("Talon")}>
+        <img src={Talon} alt="Imagen 13"/>
+        </Link>
+      </div>
+      <div>
+      <Link to={`/detail/${encodeURI("Vladimir")}`} onClick={() => handleDetailClick("Vladimir")}>
+        <img src={Vladimir} alt="Imagen 14"/>
+        </Link>
+      </div>   
+    </Slider>
+  );
+};
+
+export default Noxus;
--- Front/src/Views/Maps.js
+++ Front/src/Views/Maps.js
@@ -50,6 +50,7 @@ import Demacia from "../Components/Carrousel/region/demacia";
 import Freljord from "../Components/Carrousel/region/freljord";
 import Ionia from "../Components/Carrousel/region/ionia";
 import Ixtal from "../Components/Carrousel/region/ixtal";
+import Noxus from "../Components/Carrousel/region/noxus";
 
 function Maps() {
   const [informacion, setInformacion] = useState(""); // Estado para almacenar la información
@@ -103,7 +104,7 @@ function ocultarDivYBotones() {
         imagenes: [ Nox1, Nox2, Nox3],      
         titulo: "NOXUS",
         subtitulo: "BRUTAL EXPANSIONIST EMPIRE",
-        carr:"carrousel de NOXUS",
+        carr:<Noxus/>,
       },
     Shurima:{
         texto: "The empire of Shurima was once a thriving civilization that spanned an entire continent. Forged in a bygone age by the mighty god-warriors of the Ascended Host, it united all the disparate peoples of the south, and enforced a lasting peace between them. Few dared to rebel. Those that did, like the accursed nation of Icathia, were crushed without mercy. However, after several thousand years of growth and prosperity, the failed Ascension of Shurima’s last emperor left the capital in ruins, and tales of the empire’s former glory became little more than myth. Now, most of the nomadic inhabitants of Shurima’s deserts eke out a meager existence from the unforgiving land. Some have built small outposts to defend the few oases, while others delve into long lost catacombs in search of the untold riches that must surely lay buried there. There are also those who live as mercenaries, taking coin for their service before disappearing back into the lawless wastelands. Still, a handful dare to dream of a return to the old ways. Indeed, more recently the tribes have been stirred by whispers from the heart of the desert—that their emperor Azir has returned, to lead them into a new, wondrous age.",

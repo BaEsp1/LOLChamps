b38eeed35a352bc787d42ebba6df42d499254027
Bárbara Espinola <Baesp2023@gmail.com>
2023-11-27 20:17:09 -0300

    LP - Responsive
    max-width: 767px

--- Front/src/Components/Carrousel/Lp-Carrousel.jsx
+++ Front/src/Components/Carrousel/Lp-Carrousel.jsx
@@ -27,6 +27,22 @@ const MyCarousel = () => {
     speed: 500,
     slidesToShow: 3, 
     slidesToScroll: 3,
+     responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   };
 
   return (

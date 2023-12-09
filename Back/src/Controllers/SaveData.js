e299d866d1518ef81c296eea80611c1775a9ebec
Bárbara Espinola <Baesp2023@gmail.com>
2023-12-09 17:51:58 -0300

    Update SaveData.js

--- Back/src/Controllers/SaveData.js
+++ Back/src/Controllers/SaveData.js
@@ -2,29 +2,25 @@ const { Champ } = require('../db');
 const axios = require('axios');
 
 async function getData() {
-  const allChamps = await Champ.findAll();
+  try {
+    const apiResponse = await axios.get('http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json');
+    const apiChamps = Object.values(apiResponse.data.data).map((e) => {
+      return {
+        id: e.id,
+        name: e.name,
+        title: e.title,
+        blurb: e.blurb,
+        image: e.image.full,
+        tags: e.tags,
+        info: e.info,
+      };
+    });
 
-  if (!allChamps.length) {
-    try {
-      const apiResponse = await axios.get('http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json');
-      const apiChamps = Object.values(apiResponse.data.data).map((e) => {
-        return {
-          id: e.id,
-          name: e.name,
-          title: e.title,
-          blurb: e.blurb,
-          image: e.image.full,
-          tags: e.tags,
-          info: e.info,
-        };
-      });
-
-      await Champ.sync({ force: true }); // Esto eliminará y recreará la tabla Champ
-      await Champ.bulkCreate(apiChamps);
-      console.log('BD created');
-    } catch (error) {
-      console.error('Error fetching or processing data:', error.message);
-    }
+    await Champ.sync({ force: true }); // Esto eliminará y recreará la tabla Champ
+    await Champ.bulkCreate(apiChamps);
+    console.log('BD created');
+  } catch (error) {
+    console.error('Error fetching or processing data:', error.message);
   }
 
   const updatedChamps = await Champ.findAll();

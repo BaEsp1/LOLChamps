ff534bdb397e6c0d7e889b925a43664509111f0f
Bárbara Espinola <Baesp2023@gmail.com>
2023-12-09 17:55:17 -0300

    Update SaveData.js

--- Back/src/Controllers/SaveData.js
+++ Back/src/Controllers/SaveData.js
@@ -16,15 +16,13 @@ async function getData() {
       };
     });
 
-    await Champ.sync({ force: true }); // Esto eliminará y recreará la tabla Champ
+
     await Champ.bulkCreate(apiChamps);
     console.log('BD created');
   } catch (error) {
     console.error('Error fetching or processing data:', error.message);
   }
 
-  const updatedChamps = await Champ.findAll();
-  return updatedChamps;
 }
 
 module.exports = { getData };
